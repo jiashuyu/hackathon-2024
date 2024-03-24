c158398e999b50dc3351cdeb78252e3461a9dce6
jiashuyu <jiashuyugenius@gmail.com>
2024-03-24 09:30:26 -0500

    title

--- static/index.js
+++ static/index.js
@@ -571,7 +571,7 @@ function SplashScreen(props) {
             <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
                 <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
                     <h1 className="display-4 fw-bold lh-1 text-body-emphasis">HackaChat</h1>
-                    <p className="lead">A Chat Room created for Hackathon 2024.</p>
+                    <p className="lead">A Communication App Designed for Small-Group created in Hackathon 2024.</p>
                     <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
                         {props.user ? (
                             <button type="button" className="btn btn-primary btn-lg px-4 me-md-2 fw-bold"
